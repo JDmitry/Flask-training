679d1694cbb354aae142f0ae7ca98414b59ce7ca
Dmitry Sysoev <jdmitrys@yandex.ru>
2022-11-28 21:53:23 +0400

    Add first app

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+print(app.url_map)
+
+@app.route('/')
+def index():
+    return "<h1>Python</h1>"
+
+# app.add_url_rule('/', 'index', index)
+
+@app.route("/about")
+def about():
+    return "<h2>About</h2>"
+
+# app.add_url_rule("/about", "about", about)
+
+@app.route('/feedback')
+@app.route('/contact')
+def feedback():
+    return "Feedback"
+
+# app.add_url_rule("/feedback", 'feed', feedback)
+# app.add_url_rule("/contact", "con", feedback)
+
+@app.route('/user/<int:id>/')
+def user(id):
+    return "User #{}".format(id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
