de8b938da1a26f27452136d7494a9c88de6e9620
Dmitry Sysoev <jdmitrys@yandex.ru>
2022-11-29 13:55:07 +0400

    Context

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
-# app.debug=True
 
-@app.route('/')
-def index():
-    return "<h1>Python</h1>"
-
-# app.add_url_rule('/', 'index', index)
-
-@app.route("/about")
-def about():
-    return "<h2>About</h2>"
-
-# app.add_url_rule("/about", "about", about)
-
-@app.route('/feedback')
-@app.route('/contact')
-def feedback():
-    return "Feedback"
-
-# app.add_url_rule("/feedback", 'feed', feedback)
-# app.add_url_rule("/contact", "con", feedback)
-
-@app.route('/user/<int:id>/')
-def user(id):
-    return "User #{}".format(id)
+@app.route("/")
+def req():
+    return "IP: {}\nother: {}".format(request.remote_addr, request.user_agent)
 
 if __name__ == "__main__":
     app.run(debug=True)
